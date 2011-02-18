1e505453c5231b281f52e3283b035c1de2330fc6
Steffen Römer <steffen-roemer@users.noreply.github.com>
2011-02-18 01:37:54 +0000

    ending check function

--- orkige_engine/engine_util/StringUtil.h
+++ orkige_engine/engine_util/StringUtil.h
@@ -32,6 +32,18 @@ namespace Orkige
 			}
 			return utfString;
 		}
+		//! check if given string has given ending
+		static inline bool hasEnding (String const & fullString, String const & ending)
+		{
+			if (fullString.length() > ending.length()) 
+			{
+				return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+			} 
+			else 
+			{
+				return false;
+			}
+		}
 	}
 	//---------------------------------------------------------
 }
