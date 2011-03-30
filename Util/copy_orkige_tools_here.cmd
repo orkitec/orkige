ac833a592a6729eb1f1a694aba8d41de1aa390ff
Christoph Ender <christoph.ender@f82a661f-847e-4129-904f-941b1d0a6ad4>
2011-03-30 15:29:08 +0000

    added script for conveniently copy tools executable files

---
+++ Util/copy_orkige_tools_here.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+echo copying menuviewer
+copy ..\..\..\build\Win32\orkige_menuviewer\release\*.exe .
+
+echo copying fontconverter
+copy ..\..\..\build\Win32\orkige_fontconverter\release\*.exe .
+
+pause
