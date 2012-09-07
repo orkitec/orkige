afa82a398815fc71558e5b1f25e8f917d9b88c47
Steffen Römer <steffen-roemer@users.noreply.github.com>
2012-09-08 00:39:52 +0200

    android changes to latest ndk

---
+++ configure-android-osx.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+export MAINFOLDER=`pwd`
+
+export OPTIONS=("$MAINFOLDER" -G "Unix Makefiles"
+    -DOGRE_BUILD_PLATFORM_ANDROID=1
+    -DCMAKE_TOOLCHAIN_FILE="$MAINFOLDER/CMake/Android/android.toolchain"
+    -DORKIGE_BUILD_ANDROID=1 
+    -DORKIGE_MINIMAL_FREEIMAGE_CODEC=1
+)
+
+#export NDK=/Users/Steffen/Development/rost/android-ndk-r8b
+
+if [ "" = "$1" ]; 
+then
+	echo "Use: $0 <build target directory> [ debug ]"
+	exit 1
+fi
+
+if [ "" = "${NDK}" ]; 
+then
+	echo "NDK Path is empty."
+	exit 1
+fi
+
+export NDK_BIN=$NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/darwin-x86/bin
+export PATH=$PATH:$NDK_BIN
+
+if [ ! -d $1 ]; 
+then
+	mkdir "$1"
+fi
+
+if [ ! -d $1/orkige ]; 
+then
+	mkdir "$1/orkige"
+fi
+
+#
+cd "$1" #>/dev/null 2>&1 && 
+cd orkige
+
+ls
+
+# It's necessary to run cmake twice in order to generate files needed for RTSS
+if [ "debug" = "$2" ];
+then
+cmake "${OPTIONS[@]}" -DORKIGE_ANDROID_DEBUG=1 -DCMAKE_BUILD_TYPE="Debug" --debug-trycompile && \
+cmake "${OPTIONS[@]}" -DORKIGE_ANDROID_DEBUG=1 -DCMAKE_BUILD_TYPE="Debug" --debug-trycompile
+else
+cmake "${OPTIONS[@]}" -DCMAKE_BUILD_TYPE="MinSizeRel" && \
+cmake "${OPTIONS[@]}" -DCMAKE_BUILD_TYPE="MinSizeRel"
+fi
+
+#make
